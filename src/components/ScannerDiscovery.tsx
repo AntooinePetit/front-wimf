0b55af652035a6d73c393e22c88a3b059820f5cc
Antoine Petit <antooine.petit@gmail.com>
2025-11-04 16:23:41 +0100

    feat(scanner): add mobile only message
    - Added mobile check with useEffect
    - Displayed message on mobile
    - Increased article gap

--- src/components/ScannerDiscovery.tsx
+++ src/components/ScannerDiscovery.tsx
@@ -1,7 +1,17 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "../styles/components/ScannerDiscovery.scss";
 
 const ScannerDiscovery = () => {
+  const [isMobile, setIsMobile] = useState<boolean>(window.innerWidth <= 767);
+
+  useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth <= 767);
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <section id="scanner-discovery" className="container">
       <h2>Découvre notre outil scanner</h2>
@@ -25,6 +35,8 @@ const ScannerDiscovery = () => {
           peut t’en créer une sur mesure. Et si la recette te plaît, partage-la
           avec nous pour qu’elle ait une chance de rejoindre notre catalogue !
         </p>
+
+        {isMobile && <p>Exclusivement disponible sur mobile !</p>}
       </article>
 
       <Link to={"/scanner"} className="button">
