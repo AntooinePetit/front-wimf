eaa4c4c49295a8fce26bae5c78473f94cacbcfe9
Antoine Petit <antooine.petit@gmail.com>
2025-11-13 16:56:00 +0100

    feat: implement basic camera capture
    - Added camera view
    - Implemented capture functionality
    - Display captured image

--- src/components/CameraScanner.tsx
+++ src/components/CameraScanner.tsx
@@ -1,12 +1,14 @@
-import { useCallback, useRef } from "react";
+import { useCallback, useRef, useState } from "react";
 import Webcam from "react-webcam";
 
 const CameraScanner = () => {
+  const [image, setImage] = useState()
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const webcamRef = useRef<any>(null);
   const capture = useCallback(() => {
     if (webcamRef.current) {
       const imageSrc = webcamRef.current.getScreenshot();
+      setImage(imageSrc);
     }
   }, [webcamRef]);
 
@@ -22,6 +24,7 @@ const CameraScanner = () => {
       <button id="reject-photo" />
       <button id="take-photo" onClick={capture}/>
       <button id="validate-photo" />
+      <img src={image} alt="Test" />
     </section>
   );
 };
