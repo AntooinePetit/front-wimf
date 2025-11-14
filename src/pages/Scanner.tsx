5bd4d53fefd01540dadb1bfb832ce9e2abce7adc
Antoine Petit <antooine.petit@gmail.com>
2025-11-14 12:12:42 +0100

    feat(scanner): Add manual input link & styling
    - Added link to manual input
    - Styled scanner page

--- src/pages/Scanner.tsx
+++ src/pages/Scanner.tsx
@@ -1,16 +1,22 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import CameraScanner from "../components/CameraScanner";
 import NavBar from "../components/NavBar";
+import "../styles/pages/Scanner.scss";
 
 const Scanner = () => {
   const [showCamera, setShowCamera] = useState(false);
 
   return (
     <>
-      <main>
+      <main id="scanner" className="container">
         <button className="button" onClick={() => setShowCamera(true)}>
           Scanner mon frigo
         </button>
+
+        <Link to={"/"} className="button">
+          Entrer mes ingrédients manuellement
+        </Link>
 
         {showCamera && <CameraScanner setShowCamera={setShowCamera} />}
       </main>
