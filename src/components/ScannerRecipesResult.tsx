5db8ce92332bfe036407c88d4e8bb30b8edded8a
Antoine Petit <antooine.petit@gmail.com>
2025-11-20 11:38:57 +0100

    feat(scanner): implement load more for recipes
    - Added "Afficher plus" button
    - Limited initial display to 8
    - Added useState for shown recipes

--- src/components/ScannerRecipesResult.tsx
+++ src/components/ScannerRecipesResult.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { config } from "../config";
 import "../styles/components/ScannerRecipesResults.scss";
@@ -13,19 +14,36 @@ interface ScannerRecipesResultsProps {
 }
 
 const ScannerRecipesResults = ({ recipes }: ScannerRecipesResultsProps) => {
+  const [recipesShown, setRecipesShown] = useState(8);
+
   return (
     <div id="scanner-recipes-result">
-      {recipes.map((recipe) => (
-        <Link to={`/recipes/recipe/${recipe.id_recipe}`} key={recipe.id_recipe}>
-          <article>
-            <img
-              src={`${config.apiUrl}/uploads/recipes/${recipe.image_recipe}`}
-              alt={`Photo de ${recipe.name_recipe.toLowerCase()}`}
-            />
-            <h3>{recipe.name_recipe}</h3>
-          </article>
-        </Link>
-      ))}
+      <div>
+        {recipes.slice(0, recipesShown).map((recipe) => (
+          <Link
+            to={`/recipes/recipe/${recipe.id_recipe}`}
+            key={recipe.id_recipe}
+          >
+            <article>
+              <img
+                src={`${config.apiUrl}/uploads/recipes/${recipe.image_recipe}`}
+                alt={`Photo de ${recipe.name_recipe.toLowerCase()}`}
+              />
+              <h3>{recipe.name_recipe}</h3>
+            </article>
+          </Link>
+        ))}
+      </div>
+      {recipesShown < recipes.length && (
+        <button
+          className="button"
+          onClick={() =>
+            setRecipesShown((prev) => Math.min(prev + 8, recipes.length))
+          }
+        >
+          Afficher plus de résultats
+        </button>
+      )}
     </div>
   );
 };
